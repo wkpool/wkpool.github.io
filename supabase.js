8bcf2907053524a1bc810cc526a3c4302c5b9ca6
nielsvt <nielsvt@users.noreply.github.com>
2026-05-16 13:26:43 +0200

    removed match sql

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const SUPABASE_URL = 'https://jqomnywegtyvgnxtbnjl.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impxb21ueXdlZ3R5dmdueHRibmpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4OTc2OTUsImV4cCI6MjA5NDQ3MzY5NX0.VjOXsDHRcDrXO2EaZPTLzw5iBSlHFan9FrNW2HJO2cY
+const SUPABASE_ANON_KEY ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impxb21ueXdlZ3R5dmdueHRibmpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4OTc2OTUsImV4cCI6MjA5NDQ3MzY5NX0.VjOXsDHRcDrXO2EaZPTLzw5iBSlHFan9FrNW2HJO2cY'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
